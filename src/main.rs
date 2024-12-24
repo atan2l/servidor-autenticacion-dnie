35055057d52de040a3d0e059d5a3ab4d6a5b47be
Carlos Amores <cadenaskarlos@gmail.com>
2024-12-24 15:37:39 +0100

    Enable client certificate verification with custom roots.
    This commit introduces client certificate verification using a custom root certificate store. Certificates `ACRaiz.crt` and `ACRaiz2.crt` are added for this purpose. The `create_root_cert_store` function is implemented to initialize the RootCertStore with the specific certificates, and the configuration now enforces client authentication via `WebPkiClientVerifier`.

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,22 @@ mod auth;
 use axum::routing::get;
 use axum::Router;
 use axum_server::tls_rustls::RustlsConfig;
-use rustls::ServerConfig;
+use rustls::server::WebPkiClientVerifier;
+use rustls::{RootCertStore, ServerConfig};
 use rustls_pki_types::pem::PemObject;
 use rustls_pki_types::{CertificateDer, PrivateKeyDer};
+use std::fs::read;
 use std::net::SocketAddr;
 use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let root_cert_store = create_root_cert_store();
+    let client_cert_verifier = WebPkiClientVerifier::builder(root_cert_store.into())
+        .build()
+        .unwrap();
     let config = ServerConfig::builder()
-        .with_no_client_auth()
+        .with_client_cert_verifier(client_cert_verifier)
         .with_single_cert(
             vec![CertificateDer::from_pem_file("certs/localhost.crt").unwrap()],
             PrivateKeyDer::from_pem_file("certs/localhost.key").unwrap(),
@@ -21,9 +26,22 @@ async fn main() {
         .unwrap();
     let config = RustlsConfig::from_config(Arc::new(config));
 
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum_server::bind_rustls(addr, config)
         .serve(app.into_make_service())
         .await
         .unwrap()
+}
+
+fn create_root_cert_store() -> RootCertStore {
+    let mut root_cert_store = RootCertStore::empty();
+    root_cert_store
+        .add(CertificateDer::from(read("certs/AC005.crt").unwrap()))
+        .unwrap();
+    root_cert_store
+        .add(CertificateDer::from(read("certs/ACRaiz2.crt").unwrap()))
+        .unwrap();
+
+    root_cert_store
 }
