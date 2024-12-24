98e6cce040dd2de93f81aab50232b719190964b2
Carlos Amores <cadenaskarlos@gmail.com>
2024-12-24 13:38:11 +0100

    Refactor TLS setup with explicit Rustls server config
    Replaced `RustlsConfig::from_pem_file` with a more customizable `ServerConfig` for TLS setup, allowing explicit certificate and key handling. Simplified dependency versions in `Cargo.toml` for consistency and improved maintainability.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,24 @@ mod auth;
 
 use axum::routing::get;
 use axum::Router;
-use std::net::SocketAddr;
 use axum_server::tls_rustls::RustlsConfig;
+use rustls::ServerConfig;
+use rustls_pki_types::pem::PemObject;
+use rustls_pki_types::{CertificateDer, PrivateKeyDer};
+use std::net::SocketAddr;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    let config = RustlsConfig::from_pem_file("localhost.crt", "localhost.key").await.unwrap();
+    let config = ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(
+            vec![CertificateDer::from_pem_file("localhost.crt").unwrap()],
+            PrivateKeyDer::from_pem_file("localhost.key").unwrap(),
+        )
+        .unwrap();
+    let config = RustlsConfig::from_config(Arc::new(config));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum_server::bind_rustls(addr, config)
